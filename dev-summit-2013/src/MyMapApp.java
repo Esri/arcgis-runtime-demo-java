3a88d7f0c1fad57d1f03d12a168cf37c4fe2205f
Vijay Gandhi <vgandhi@esri.com>
2013-04-11 14:28:21 -0700

    Added GPS Layer.
    - GPSLayer is a type to read GPS data from a file or a serial port in
    NMEA format.

--- dev-summit-2013/src/MyMapApp.java
+++ dev-summit-2013/src/MyMapApp.java
@@ -2,22 +2,35 @@
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JToolBar;
 import javax.swing.SwingUtilities;
 
 import com.esri.core.geometry.Envelope;
+import com.esri.core.gps.FileGPSWatcher;
 import com.esri.map.ArcGISTiledMapServiceLayer;
+import com.esri.map.GPSLayer;
 import com.esri.map.JMap;
 import com.esri.map.MapEvent;
 import com.esri.map.MapEventListenerAdapter;
+import com.esri.map.GPSLayer.Mode;
 
+/**
+ * This sample shows how to create a basic map application.
+ */
 public class MyMapApp {
 
   private JFrame window;
   private JMap map;
+  
+  private FileGPSWatcher watcher;
+  private GPSLayer gpsLayer;
 
   public MyMapApp() {
     window = new JFrame();
@@ -50,11 +63,40 @@ public class MyMapApp {
     });
 
     window.getContentPane().add(map, BorderLayout.CENTER);
+    
+    // create toolbar
+    JToolBar toolbar = new JToolBar();
+    window.getContentPane().add(toolbar, BorderLayout.NORTH);
+
+    // Start/stop GPS button
+    JButton btnLocation = new JButton("GPS on/off");
+    toolbar.add(btnLocation);
+
+    btnLocation.addActionListener(new ActionListener() {
+
+      @Override
+      public void actionPerformed(ActionEvent arg0) {
+        if (!map.getLayers().contains(gpsLayer)) {
+          addGPSLayer();
+        } else {
+          map.getLayers().remove(gpsLayer);
+        }
+      }
+    });
 
     // create and add tiled layer
     ArcGISTiledMapServiceLayer tiledLayer = new ArcGISTiledMapServiceLayer(
         "http://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer");
     map.getLayers().add(tiledLayer);
+  }
+  
+  private void addGPSLayer() {
+    watcher = new FileGPSWatcher("data" + System.getProperty("file.separator") + "meadows.txt", 800, true);
+    gpsLayer = new GPSLayer(watcher);
+    gpsLayer.setMode(Mode.OFF);
+    gpsLayer.setShowTrackPoints(false);
+    gpsLayer.setShowTrail(false);
+    map.getLayers().add(gpsLayer);
   }
   
   /**
