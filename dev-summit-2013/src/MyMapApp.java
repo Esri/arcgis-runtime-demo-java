a7c8121717d5d7a0259c7d1ae7ca058ef964ee8e
Vijay Gandhi <vgandhi@esri.com>
2013-04-11 14:34:57 -0700

    Added geocoding service.
    - Type Locator is used to geocode using a online geocoding service.
    - Result from the service is added as a graphic to a GraphicsLayer.

--- dev-summit-2013/src/MyMapApp.java
+++ dev-summit-2013/src/MyMapApp.java
@@ -6,16 +6,31 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 import javax.swing.JToolBar;
 import javax.swing.SwingUtilities;
 
 import com.esri.core.geometry.Envelope;
+import com.esri.core.geometry.Point;
 import com.esri.core.gps.FileGPSWatcher;
+import com.esri.core.map.CallbackListener;
+import com.esri.core.map.Graphic;
+import com.esri.core.symbol.PictureMarkerSymbol;
+import com.esri.core.symbol.Symbol;
+import com.esri.core.tasks.ags.geocode.Locator;
+import com.esri.core.tasks.ags.geocode.LocatorFindParameters;
+import com.esri.core.tasks.ags.geocode.LocatorGeocodeResult;
 import com.esri.map.ArcGISTiledMapServiceLayer;
 import com.esri.map.GPSLayer;
+import com.esri.map.GraphicsLayer;
 import com.esri.map.JMap;
 import com.esri.map.MapEvent;
 import com.esri.map.MapEventListenerAdapter;
@@ -31,6 +46,10 @@ public class MyMapApp {
   
   private FileGPSWatcher watcher;
   private GPSLayer gpsLayer;
+  
+  private JTextField textField;
+  private GraphicsLayer addressGraphics;
+  private Symbol geocodeSymbol;
 
   public MyMapApp() {
     window = new JFrame();
@@ -88,6 +107,83 @@ public class MyMapApp {
     ArcGISTiledMapServiceLayer tiledLayer = new ArcGISTiledMapServiceLayer(
         "http://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer");
     map.getLayers().add(tiledLayer);
+    
+    // GEOCODING
+    addressGraphics = new GraphicsLayer();
+    map.getLayers().add(addressGraphics);
+    geocodeSymbol = createGeocodeSymbol();
+    
+    // Geocoding
+    textField = new JTextField("100 holyrood, Edinburgh");
+    JButton findButton = new JButton("Find");
+
+    toolbar.add(textField);
+    toolbar.add(findButton);
+
+    findButton.addActionListener(new ActionListener() {
+
+      @Override
+      public void actionPerformed(ActionEvent arg0) {
+        onFind();
+      }
+    });
+  }
+  
+  protected void onFind() {
+    Locator locator = new Locator(
+        "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer");
+    LocatorFindParameters params = new LocatorFindParameters(textField.getText());
+    params.setOutSR(map.getSpatialReference());
+
+    // additional parameters optionally, could grab the latest point from a GPS feed for example
+    params.setLocation(new Point(-356903.5435, 7546014.500), map.getSpatialReference());
+    params.setDistance(10000);
+
+    // run the locator task asynchronously
+    locator.findAsync(params, new CallbackListener<List<LocatorGeocodeResult>>() {
+
+      @Override
+      public void onError(Throwable e) {
+        JOptionPane.showMessageDialog(map.getParent(), e.getMessage());
+      }
+
+      @Override
+      public void onCallback(List<LocatorGeocodeResult> results) {
+        // display top result
+        if (results != null) {
+          // get the top result to display on map
+          LocatorGeocodeResult highestScoreResult = results.get(0);
+
+          // create and populate attribute map
+          Map<String, Object> attributes = new HashMap<String, Object>();
+          for (Entry<String, String> entry : highestScoreResult.getAttributes().entrySet())
+          {
+            attributes.put(entry.getKey(), entry.getValue());
+          }
+
+          // create a graphic at this location
+          Graphic addressGraphic = new Graphic(
+              highestScoreResult.getLocation(), 
+              geocodeSymbol, 
+              attributes, 
+              null);
+          addressGraphics.addGraphic(addressGraphic);
+
+          // centre the map at this location
+          Envelope extent = map.getExtent();
+          extent.centerAt(highestScoreResult.getLocation());
+          map.zoomTo(extent);
+        }
+      }
+    });
+  }
+  
+  private PictureMarkerSymbol createGeocodeSymbol() {
+    PictureMarkerSymbol symPoint = new PictureMarkerSymbol(
+        "http://static.arcgis.com/images/Symbols/Basic/RedShinyPin.png");
+    symPoint.setSize(40, 40);
+    symPoint.setOffsetY(20.0f);
+    return symPoint;
   }
   
   private void addGPSLayer() {
